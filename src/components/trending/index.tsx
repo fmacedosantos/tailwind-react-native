09a3544beeae3faea91db83d05b329e63421ad1a
fmacedosantos <felipemacedo986@gmail.com>
2024-09-23 15:40:32 -0300

    Finalizando os trending foods

--- src/components/trending/index.tsx
+++ src/components/trending/index.tsx
@@ -2,8 +2,19 @@ import { useEffect, useState } from 'react';
 import { FlatList, View } from 'react-native';
 import CardHorizontalFood from './food';
 
+export interface FoodProps{
+    id: string
+    name: string
+    price: number
+    time: string
+    delivery: number
+    rating: number
+    image: string
+    restaurantId: string
+}
+
 export function TrendingFoods() {
-    const [foods, setFoods] = useState([])
+    const [foods, setFoods] = useState<FoodProps[]>([])
 
     useEffect(() => {
     async function getFoods() {
@@ -17,8 +28,10 @@ export function TrendingFoods() {
  return (
    <FlatList
     data={foods}
-    renderItem={({item}) => <CardHorizontalFood/>}
+    renderItem={({item}) => <CardHorizontalFood food={item}/>}
     horizontal={true}
+    contentContainerStyle={{gap: 14, paddingLeft: 16, paddingRight: 16}}
+    showsHorizontalScrollIndicator={false}
    />
   );
 }
